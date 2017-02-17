1fd99850426f84a80406521eb07be5ef912ff38a
zeyni <zeyni@ZEN3>
2017-02-17 10:49:04 -0500

    Add project files.

---
+++ BarrowIt/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BarrowIt.Startup))]
+namespace BarrowIt
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
